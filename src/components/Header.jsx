60452ebc23cc88aa15ce0f6263cf0673a8545b1e
Lucas Choi <wonbo1.dev@gmail.com>
2025-05-19 12:18:27 -0300

    add burguer menu to mobile version

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,15 @@
+import { useState } from "react";
 import { Link, animateScroll as scroll } from "react-scroll";
 import logo from "../assets/logo.svg";
 import styles from "./Header.module.css";
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleNavClick = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
@@ -11,25 +18,63 @@ const Header = () => {
         </a>
       </div>
 
-      <nav>
-        <ul className={styles.navbar}>
+      <button
+        className={`${styles.burger} ${menuOpen ? styles.open : ""}`}
+        aria-label="Abrir menu"
+        aria-expanded={menuOpen}
+        onClick={() => setMenuOpen((open) => !open)}
+      >
+        <span className={styles.burgerBar}></span>
+        <span className={styles.burgerBar}></span>
+        <span className={styles.burgerBar}></span>
+      </button>
+
+      <nav className={`${styles.navbar} ${menuOpen ? styles.open : ""}`}>
+        <ul>
           <li>
-            <Link to="about" smooth={true} duration={500} offset={-250} className={styles.navigation}>
+            <Link
+              to="about"
+              smooth={true}
+              duration={500}
+              offset={-250}
+              className={styles.navigation}
+              onClick={handleNavClick}
+            >
               <span>01.</span> Sobre
             </Link>
           </li>
           <li>
-            <Link to="experiences" smooth={true} duration={500} offset={-250} className={styles.navigation}>
+            <Link
+              to="experiences"
+              smooth={true}
+              duration={500}
+              offset={-250}
+              className={styles.navigation}
+              onClick={handleNavClick}
+            >
               <span>02.</span> Experiência
             </Link>
           </li>
           <li>
-            <Link to="projects" smooth={true} duration={500} offset={-250} className={styles.navigation}>
+            <Link
+              to="projects"
+              smooth={true}
+              duration={500}
+              offset={-250}
+              className={styles.navigation}
+              onClick={handleNavClick}
+            >
               <span>03.</span> Projetos
             </Link>
           </li>
           <li>
-            <Link to="contact" smooth={true} duration={500} className={styles.navigation}>
+            <Link
+              to="contact"
+              smooth={true}
+              duration={500}
+              className={styles.navigation}
+              onClick={handleNavClick}
+            >
               <span>04.</span> Contato
             </Link>
           </li>
